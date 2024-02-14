28efec3d93ee8d8273b8df728507b87f920e8302
Yan Moroz <mail@yanmoroz.ru>
2024-02-14 18:47:35 +0300

    Initial Commit

---
+++ Earthquakes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Earthquakes
+//
+//  Created by Yan Moroz on 14.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Yan Moroz on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
